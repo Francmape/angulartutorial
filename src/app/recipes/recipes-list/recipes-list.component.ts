eef97b28037d098e929fd658b117a1b246cd8b56
Francis Akilimali <francmape@gmail.com>
2022-08-10 23:52:20 +0300

    Learning curve

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] =[
+    new Recipe("Sima", "Maize flour na maji","https://grubbys.africa/wp-content/uploads/2021/08/Ugali-1.jpg"),
+    new Recipe("Wali", "Mpunga wa kupaka","https://media.istockphoto.com/photos/rice-in-a-bowl-on-a-white-background-picture-id860931464?k=20&m=860931464&s=612x612&w=0&h=Q5ADqpZbQbVzm3YkNwbyhn023S64em9w08O0xg0b0KE=")
+  ];
 
   constructor() { }
 
